8533aa645f28eee098cfeb708b19932b522fd7a4
YevhDr <evgesha.drozdova@gmail.com>
2022-02-18 19:06:59 +0200

    book slider

--- js/main.js
+++ js/main.js
@@ -1,26 +1,50 @@
-//слайдер-горталка
-var splide = new Splide('.splide', {
-     //padding: '5rem',
+
+
+d3.csv('data/books.csv').then(function(books){
+    
+    
+    //слайдер-горталка    
+    var splide = new Splide('.splide', {    
     type  : 'fade',
     rewind: true
-});
+    });
 
-splide.mount();
+    splide.mount();
 
 
-document.getElementById('close').addEventListener('click', function(){    
-    document.getElementById('books-wrapper').style.display='none';
-})
-
-var booksList = document.getElementsByClassName('open-modal');
-console.log(booksList);
-for(var i = 0; i < booksList.length; i++){
-    var button = booksList[i]   
-    button.addEventListener('click', function(){
-       /*  document.querySelector('body').style.height='100vh';
-        document.querySelector('body').style.overflow='hidden'; */
-        document.getElementById('books-wrapper').style.display='flex';        
+    d3.select('#close').on('click', function(){    
+        d3.select('#books-wrapper').style('display','none');
     })
 
-}
+    d3.selectAll('.open-modal').on('click', function(){
+
+        let id = d3.select(this).attr('data-value');
+        let slide = books.filter(function(d){ 
+            return d.id === id 
+        });
+
+        let group = books.filter(function(d){ 
+            return d.group === slide[0].group && d.id != id 
+        })
+        
+        while (splide.length > 0) {
+            splide.remove(splide.length - 1);
+        }        
+
+        splide.add(slide[0].html);  
+        
+        group.forEach(function(el){
+            splide.add(el.html);
+        })
+
+        d3.select('#books-wrapper').style('display','flex');
+
+        
+    })
+    
+    
+
+});
+
+
 
